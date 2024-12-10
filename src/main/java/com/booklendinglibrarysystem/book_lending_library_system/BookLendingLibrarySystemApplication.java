e41a19106a18d962c290388df92c8b4f757b9642
singhHarcharan <gurcharansinghkhalsa262@gmail.com>
2024-12-10 10:58:16 +0530

    first commit

---
+++ src/main/java/com/booklendinglibrarysystem/book_lending_library_system/BookLendingLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.booklendinglibrarysystem.book_lending_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLendingLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLendingLibrarySystemApplication.class, args);
+	}
+
+}
